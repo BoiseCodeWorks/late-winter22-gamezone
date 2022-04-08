7f3891d24ae91a4697a244eb1f57f69c18910ba8
mick shannahan <beasticorn@gmail.com>
2022-04-08 12:30:23 -0600

    create many to many

--- dbSetup.sql
+++ dbSetup.sql
@@ -16,6 +16,14 @@ CREATE TABLE IF NOT EXISTS games(
   recommendedPlayers INT DEFAULT 1,
   FOREIGN KEY (creatorId) REFERENCES accounts(id)
 ) default charset utf8 COMMENT '';
+CREATE TABLE IF NOT EXISTS gamePlayers(
+  id INT AUTO_INCREMENT primary key,
+  gameId INT NOT NULL,
+  accountId VARCHAR(255) NOT NULL,
+  score INT DEFAULT 0,
+  FOREIGN KEY (gameId) REFERENCES games(id) ON DELETE CASCADE,
+  FOREIGN Key (accountId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8 COMMENT '';
 DROP TABLE IF EXISTS games;
 ALTER TABLE
   games
@@ -48,3 +56,49 @@ WHERE
   id = 18
 LIMIT
   1;
+INSERT INTO
+  gameplayers (gameId, accountId, score)
+VALUES
+  (2, "6234ac00abca50735a3c9205", 50);
+SELECT
+  *
+FROM
+  gameplayers;
+SELECT
+  *
+FROM
+  gameplayers
+WHERE
+  gameId = 2;
+SELECT
+  a.name,
+  a.picture,
+  gp.score,
+  gp.id AS gamePlayerId
+FROM
+  gameplayers gp
+  JOIN accounts a ON gp.accountId = a.id
+WHERE
+  gp.gameId = 1;
+-- GET MY ACCOUNT GAMES
+SELECT
+  a.name,
+  gp.score,
+  g.name
+FROM
+  gameplayers gp
+  JOIN accounts a ON gp.accountId = a.id
+  JOIN games g ON gp.gameId = g.id
+WHERE
+  gp.accountId = "6234ac00abca50735a3c9205";
+-- GET GAME ACCOUNTS
+SELECT
+  a.name,
+  gp.score,
+  g.name
+FROM
+  gameplayers gp
+  JOIN accounts a ON gp.accountId = a.id
+  JOIN games g ON gp.gameId = g.id
+WHERE
+  gp.gameId = 1;
